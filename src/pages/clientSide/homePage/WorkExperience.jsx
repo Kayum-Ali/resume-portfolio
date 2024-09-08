902859db83df315aaa7898307aee66406cc5accd
Kayum Ali <sisirfahimstudent173@gmail.com>
2024-09-09 01:42:51 +0600

    work experience section done

--- src/pages/clientSide/homePage/WorkExperience.jsx
+++ src/pages/clientSide/homePage/WorkExperience.jsx
@@ -1,51 +1,58 @@
-
-
 const WorkExperience = () => {
-    return (
-        <div className='w-11/12 mx-auto  my-5'>
-            <p className='text-5xl font-bold text-center py-5'>My <span className='text-[#fd853a]'>Work Experience</span></p>
+  return (
+    <div className="w-11/12 mx-auto  my-5">
+      <p className="text-5xl font-bold text-center py-5">
+        My <span className="text-[#fd853a]">Work Experience</span>
+      </p>
 
-            <div className='flex justify-between mt-5 gap-12'>
-                <div className='space-y-28 w-1/3'>
-                    <div>
-                        <p className='text-4xl font-bold'>ABC Company</p>
-                        <p className='text-gray-300'>2023 to present</p>
-                    </div>
+      <div className="flex justify-between mt-10 gap-12">
+        <div className="space-y-20  w-full flex-1">
+          <div>
+            <p className="text-4xl font-bold">Cognizant, Mumbai</p>
+            <p className="text-gray-300 pt-3">Sep 2016- July 2020</p>
+          </div>
 
-                    <div>
-                        <p className='text-4xl font-bold'>ABC Company</p>
-                        <p className='text-gray-300'>2023 to present</p>
-                    </div>
+          <div>
+            <p className="text-4xl font-bold">Sugee Pvt limited, Mumbai</p>
+            <p className="text-gray-300 pt-3">Sep 2020- July 2023</p>
+          </div>
 
-                    <div>
-                        <p className='text-4xl font-bold'>ABC Company</p>
-                        <p className='text-gray-300'>2023 to present</p>
-                    </div>
-                </div>
-                <div className='w-1/3 flex justify-center'>
-                    <img src="https://res.cloudinary.com/dnvmj9pvk/image/upload/v1725684216/React%20class/Portfolio%20Project/d8vqb3adkarhy8xlback.png" alt="" />
-                </div>
-                <div className='space-y-28 w-1/3'>
-                    <div className=''>
-                        <p className='text-4xl font-bold'>Web Developer</p>
-                        <p className='text-gray-300 w-1/2'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis, voluptatibus?</p>
-                    </div>
-
-                    <div className=''>
-                        <p className='text-4xl font-bold'>Web Developer</p>
-                        <p className='text-gray-300 w-1/2'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis, voluptatibus?</p>
-                    </div>
-
-                    <div className=''>
-                        <p className='text-4xl font-bold'>Web Developer</p>
-                        <p className='text-gray-300 w-1/2'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis, voluptatibus?</p>
-                    </div>
-
-                    
-                </div>
-            </div>
+          <div>
+            <p className="text-4xl font-bold">Cinetstox, Mumbai</p>
+            <p className="text-gray-300 pt-3">Sep 2023</p>
+          </div>
         </div>
-    )
-}
+        <div className=" text-center h-full flex-1">
+          <ul className="steps steps-vertical -mt-16">
+            <li className="step step-primary h-[200px]"></li>
+            <li className="step h-[200px]"> </li>
+            <li className="step  step-primary h-[200px]"></li>
+           
+          </ul>
+        </div>
+        <div className="space-y-20  flex-1">
+          <div className="">
+            <p className="text-4xl font-bold">Experince Designer</p>
+            <p className="text-gray-300 pt-3">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis lacus nunc, posuere in justo vulputate, bibendum sodales 
+            </p>
+          </div>
 
-export default WorkExperience   
+          <div className="">
+            <p className="text-4xl font-bold">UI/UX Designer</p>
+            <p className="text-gray-300 pt-3">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis lacus nunc, posuere in justo vulputate, bibendum sodales 
+            </p>
+          </div>
+
+          <div className="-mt-8">
+            <p className="text-4xl font-bold">Lead UX Designer</p>
+           
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WorkExperience;
